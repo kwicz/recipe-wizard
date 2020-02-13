d341722b12d8a3a3d5f7a34fb9de040b3c476bce
Student <example@example.com>
2020-02-13 11:50:25 -0800

    add .env variables

--- src/meals-service.js
+++ src/meals-service.js
@@ -5,20 +5,18 @@ export class MealsService {
     this.ingredients = [];
     this.parameters = [];
     this.calories = 0;
-    this.apiKey = "c8d75103d8973f69bf5313e9d40f1907";
-    this.apiID = "063c9307";
     this.query = query;
   }
 
   async getMealByQuery() {
     try {
       // The API call is business logic.
-      let response = await fetch(`https://api.edamam.com/search?q=${this.query}&app_id=${this.apiID}&app_key=${this.apiKey}&from=0&to=12`);
+      let response = await fetch(`https://api.edamam.com/search?q=${this.query}&app_id=${process.env.API_KEY}&app_key=${process.env.API_ID}&from=0&to=12`);
       if (response.status != 200) {
         return false;
       }
       let jsonifiedResponse = await response.json();
-      // The getElements method is UI logic so it will need to be separated from the business logic.  
+      // The getElements method is UI logic so it will need to be separated from the business logic.
       return jsonifiedResponse;
     } catch (error) {
       return false;
